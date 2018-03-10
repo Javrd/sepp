bec91e987e00480486f857229aa451db014e4ea6
javrd <trarsheise@gmail.com>
2018-03-10 16:48:54 +0100

    Feat: Add first version of models
    Add classes, changing some things from UML domain models
    
    Ref. #21

--- artinbar/settings.py
+++ artinbar/settings.py
@@ -85,6 +85,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'mvp.Account'
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
--- mvp/models.py
+++ mvp/models.py
@@ -1,3 +1,67 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class Account(AbstractUser):
+    pass
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    description = models.CharField(max_length=500)
+    logo = models.URLField()
+    sentMessage = models.ManyToManyField('self', symmetrical=False, through='Message')
+    account = models.OneToOneField(Account, on_delete=models.CASCADE)
+
+
+class Artist(User):
+    artistNumber = models.IntegerField()
+
+
+class Venue(User):
+    geolocation = models.OneToOneField("Geolocation", on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    capactity = models.IntegerField()
+
+
+class Photo(models.Model):
+    url = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+
+class Media(models.Model):
+    url = models.URLField()
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+
+class Geolocation(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+
+class Message(models.Model):
+    timeStamp = models.DateTimeField()
+    text = models.CharField(max_length=500)
+    from_user = models.ForeignKey(User, related_name='from_user', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='to_user', on_delete=models.CASCADE)
+
+
+class Offer(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    offeredAmount = models.FloatField()
+    date = models.DateTimeField()
+
+
+class Performance(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    date = models.DateTimeField()
+    paymentAmount = models.FloatField()
+    paymentDate = models.DateTimeField()
+    public = models.BooleanField()

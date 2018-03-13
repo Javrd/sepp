4750f258361d500f2a5282525171e41aa7bc7d18
Unknown <artronluc@alum.us.es>
2018-03-13 15:09:05 +0100

    Fix: Template settings
    Directorie for templates has been stablished into project folders.

--- artinbar/settings.py
+++ artinbar/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'artinbar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'artinbar/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

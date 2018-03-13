069d928f1d77de3a317a3edc85d9d611cb58aac5
daniglper <daniglper@gmail.com>
2018-03-13 15:02:17 +0100

    Feat #32 Offer list backend

--- artinbar/urls.py
+++ artinbar/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mvp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('offerList/', offerList, name='offerList')
 ]
--- mvp/views.py
+++ mvp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
+from django.shortcuts import get_list_or_404
 
 # Create your views here.
+def offerList(request):
+    offer_list = get_list_or_404(Offer)
+    context = {'offer_list': offer_list}
+    return render(request, './offerList.html', context)

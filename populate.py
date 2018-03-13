278adc6497de1e32cf5b1d23c76e1010adba4290
roldans <roldis2@hotmail.com>
2018-03-13 11:32:08 +0100

    Feat: Populate Photos
    references #41

--- populate.py
+++ populate.py
@@ -1,9 +1,9 @@
+"""Import models from mvp/data/xxx.csv."""
 import os
-import csv
-os.environ['DJANGO_SETTINGS_MODULE'] = 'artinbar.settings'
-import sqlite3
 import django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'artinbar.settings'
 django.setup()
+import csv
 import datetime
 from mvp.models import Artist
 from mvp.models import Venue
@@ -11,78 +11,101 @@ from mvp.models import User
 from mvp.models import Account
 from mvp.models import Geolocation
 from mvp.models import Message
+from mvp.models import Photo
 
 
 def importArtists():
+    """Import artist from artists.csv to database."""
     with open('mvp/data/artists.csv') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         print(spamreader.__next__())
         for line in spamreader:
 
-            nombre=line[0]
-            email=line[1]
-            description=line[2]
-            logo=line[3]
-            integrantes=line[4]
-            artisId=line[5]
+            nombre = line[0]
+            email = line[1]
+            description = line[2]
+            logo = line[3]
+            integrantes = line[4]
+            artisId = line[5]
             print(line)
 
-            acc = Account.objects.create_user(nombre,email,nombre)
-            new = Artist.objects.create(name=nombre,description=description,
-                                        logo=logo,artistNumber=integrantes,
-                                        account=acc,id=artisId)
+            acc = Account.objects.create_user(nombre, email, nombre)
+            new = Artist.objects.create(name=nombre, description=description,
+                                        logo=logo, artistNumber=integrantes,
+                                        account=acc, id=artisId)
             new.save()
 
+
 def importVenues():
+    """Import venues from venues.csv to database."""
     with open('mvp/data/venues.csv') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         print(spamreader.__next__())
         for line in spamreader:
 
-            nombre=line[0]
-            email=line[1]
-            description=line[2]
-            logo=line[3]
-            geo=line[4]
-            address=line[5]
-            capacity=line[6]
-            venueId=line[7]
+            nombre = line[0]
+            email = line[1]
+            description = line[2]
+            logo = line[3]
+            geo = line[4]
+            address = line[5]
+            capacity = line[6]
+            venueId = line[7]
             print(line)
-            geo=geo.split('/')
-            acc = Account.objects.create_user(nombre,email,nombre)
-            geos = Geolocation.objects.create(latitude=float(geo[0]),longitude=
-                                             float(geo[1]))
-            new = Venue.objects.create(name=nombre,description=description,
-                                        logo=logo,geolocation=geos,address=
-                                        address,capacity=capacity,
-                                        account=acc,id=venueId)
+            geo = geo.split('/')
+            acc = Account.objects.create_user(nombre, email, nombre)
+            geos = Geolocation.objects.create(
+                latitude=float(geo[0]), longitude=float(geo[1]))
+            new = Venue.objects.create(name=nombre, description=description,
+                                       logo=logo, geolocation=geos,
+                                       address=address, capacity=capacity,
+                                       account=acc, id=venueId)
 
             new.save()
+
+
 def importMessages():
+    """Import messages from messages.csv to database."""
     with open('mvp/data/messages.csv') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         print(spamreader.__next__())
         for line in spamreader:
-            timeStamp=datetime.datetime.now()
-            text=line[1]
-            sender=User.objects.get(pk=line[2])
-            receiver=User.objects.get(pk=line[3])
-            messageId=line[4]
+            timeStamp = datetime.datetime.now()
+            text = line[1]
+            sender = User.objects.get(pk=line[2])
+            receiver = User.objects.get(pk=line[3])
+            messageId = line[4]
             print(line)
 
-            new = Message.objects.create(timeStamp=timeStamp,text=text,
-                                         sender=sender,receiver=receiver,id=messageId)
+            new = Message.objects.create(timeStamp=timeStamp, text=text,
+                                         sender=sender, receiver=receiver,
+                                         id=messageId)
 
             new.save()
 
 
+def importPhotos():
+    """Import photos from photos.csv to database."""
+    with open('mvp/data/photos.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        print(spamreader.__next__())
+        for line in spamreader:
+            photoUrl = line[0]
+            userId = User.objects.get(pk=line[1])
+            photoId = line[2]
+            print(line)
+            new = Photo.objects.create(url=photoUrl, user=userId, id=photoId)
+
+            new.save()
 
 
 Account.objects.all().delete()
 Artist.objects.all().delete()
 Venue.objects.all().delete()
 Message.objects.all().delete()
+Photo.objects.all().delete()
 
 importArtists()
 importVenues()
 importMessages()
+importPhotos()

72655c3d06077c73bd4a498cc015fe276073e010
Unknown <artronluc@alum.us.es>
2018-04-24 17:17:57 +0200

    Fix: Revised Test
    Added new tests and fixed errors on existing tests, also fixed tests names.
    
    closes #96

--- mvp/test/test_offers.py
+++ mvp/test/test_offers.py
@@ -22,42 +22,47 @@ class test_Offers(TestCase):
         self.assertFalse(formulario1.is_valid())
 
     def test_OfferNegativeNoDesc(self):
-        formulario1 = OfferForm(user= None,data={'name': "", 'description': "",
+        formulario1 = OfferForm(user= None,data={'name': "testOfferName", 'description': "",
                                      'offeredAmount': 10.0, 'date': "10/3/2018"})
         self.assertFalse(formulario1.is_valid())
 
     def test_OfferNegativeNoneDesc(self):
-        formulario1 = OfferForm(user= None,data={'name': "", 'description': None,
+        formulario1 = OfferForm(user= None,data={'name': "testOfferName", 'description': None,
                                      'offeredAmount': 10.0, 'date': "10/3/2018"})
         self.assertFalse(formulario1.is_valid())
 
     def test_OfferNegativeNoAmount(self):
-        formulario1 = OfferForm(user= None,data={'name': "", 'description': "this is a description",
+        formulario1 = OfferForm(user= None,data={'name': "testOfferName", 'description': "this is a description",
                                      'offeredAmount': 0.0, 'date': "10/3/2018"})
         self.assertFalse(formulario1.is_valid())
 
     def test_OfferNegativeInvalidAmount(self):
-        formulario1 = OfferForm(user= None,data={'name': "", 'description': "this is a description",
+        formulario1 = OfferForm(user= None,data={'name': "testOfferName", 'description': "this is a description",
                                      'offeredAmount': "hola", 'date': "10/3/2018"})
         self.assertFalse(formulario1.is_valid())
 
     def test_OfferNegativeNoneAmount(self):
-        formulario1 = OfferForm(user= None,data={'name': "", 'description': "this is a description",
+        formulario1 = OfferForm(user= None,data={'name': "testOfferName", 'description': "this is a description",
                                      'offeredAmount': None, 'date': "10/3/2018"})
         self.assertFalse(formulario1.is_valid())
 
     def test_OfferNegativeNoDate(self):
-        formulario1 = OfferForm(user= None,data={'name': "", 'description': "this is a description",
+        formulario1 = OfferForm(user= None,data={'name': "testOfferName", 'description': "this is a description",
                                      'offeredAmount': 10.0, 'date': ""})
         self.assertFalse(formulario1.is_valid())
 
     def test_OfferNegativeInvalidDate(self):
-        formulario1 = OfferForm(user= None,data={'name': "", 'description': "this is a description",
+        formulario1 = OfferForm(user= None,data={'name': "testOfferName", 'description': "this is a description",
                                      'offeredAmount': 10.0, 'date': "ivewvoinewve"})
         self.assertFalse(formulario1.is_valid())
 
     def test_OfferNegativeNoneDate(self):
-        formulario1 = OfferForm(user= None,data={'name': "", 'description': "this is a description",
+        formulario1 = OfferForm(user= None,data={'name': "testOfferName", 'description': "this is a description",
                                      'offeredAmount': 10.0, 'date': None})
+        self.assertFalse(formulario1.is_valid())
+
+    def test_OfferNegativeNegtiveAmount(self):
+        formulario1 = OfferForm(user= None,data={'name': "testOfferName", 'description': "this is a description",
+                                     'offeredAmount': -10.0, 'date': "10/3/2018"})
         self.assertFalse(formulario1.is_valid())
 

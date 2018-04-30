bc9c197f82f6d303e6a0b0dab4ee5e5a3de058ba
Guillermo Bandrés Catalina <gbandres98@gmail.com>
2018-04-30 12:19:30 +0200

    Fix: Paypal redirect URLs

--- mvp/views.py
+++ mvp/views.py
@@ -442,8 +442,8 @@ def payment(request):
         'transactions': transactions,
         'note_to_payer': 'Puedes contactar con nosotros para cualquier duda en pagosartinbar@gmail.com',
         'redirect_urls': {
-            'return_url': 'http://localhost:8000/artinbar',
-            'cancel_url': 'http://localhost:8000/artinbar'
+            'return_url': 'http://artinbar.es/artinbar',
+            'cancel_url': 'http://artinbar.es/artinbar'
         }
     }
 
